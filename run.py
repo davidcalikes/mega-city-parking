4915fb864ff800a86caafa1115bf0477b9ed4026
David Calikes <davidcalikes@gmail.com>
2023-10-06 22:54:55 +0100

    feat: Initialiste repo & create test script

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# simple menu to present two options to the user.
+
+def SimpleMenu():
+    """
+    Offer a simple menu that presents two options to the user.
+    """
+    user_options = ("red", "blue", "green", "yellow", "purple", "orange")
+        
+
+    while True:
+        print("Please choose from the following?")
+        print("What is your favorite colour?")
+
+        user_choice = input("Enter your data here:\n")
+
+        if user_choice in user_options:
+            print (user_choice) 
+            break
+        else:
+            print ("That is not an option you bloody idiot!!")
+            break
+
+SimpleMenu()
